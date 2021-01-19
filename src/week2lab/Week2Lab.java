ebf337e54d1f8f9b0abeef8615e870b536e3a983
834171 <834171@SAIT225667IT.ACDM.DS.SAIT.CA>
2021-01-19 14:39:00 -0700

    Hello world Printing hello world.

--- src/week2lab/Week2Lab.java
+++ src/week2lab/Week2Lab.java
@@ -16,6 +16,7 @@ public class Week2Lab {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
